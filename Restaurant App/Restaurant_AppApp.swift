30fbe47f3c29fe2ad382289153143794dcda8c7c
Antonymous1337 <holshouserantony@yahoo.com>
2024-11-08 20:32:27 -0800

    Initial Commit

---
+++ Restaurant App/Restaurant_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_AppApp.swift
+//  Restaurant App
+//
+//  Created by Antony Holshouser on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
